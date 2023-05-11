728b97ff9a3feb101602b8edb83db3991eb43293
carmobu <carlosmolanobustos@hotmail.com>
2023-05-10 19:38:20 -0500

    solucion a los ejercicios de js

---
+++ js-1/conditionals.js
@@ -0,0 +1,19 @@
+function calcularColor(num) {
+    var color;
+    if (num === 1) {
+      color = "negro";
+    } else if (num === 2) {
+      color = "blanco";
+    } else if (num === 3) {
+      color = "rojo";
+    } else {
+      color = "verde";
+    }
+  
+    return "El color es " + color;
+  }
+
+console.log(calcularColor(1))
+console.log(calcularColor(2))
+console.log(calcularColor(3))
+console.log(calcularColor(8))
